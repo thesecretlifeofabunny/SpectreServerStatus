8dc280f160d2a189a816386b46a37deeca6cee5b
thesecretlifeofabunny <212422141+thesecretlifeofabunny@users.noreply.github.com>
2025-08-22 19:18:38 -0700

    Implemented Object version of ServerPingService.cs and renamed PingService.cs to a better named file (still need to think of a better name), as well as Main(string[]) cleanup.

--- SpectreServerStatus/Program.cs
+++ SpectreServerStatus/Program.cs
@@ -4,6 +4,7 @@ using Spectre.Console;
 
 namespace SpectreServerStatus;
 
+//TODO: Allow for specifying a config of servers to ping.
 public static class Program
 {
     private const int FiveMilliseconds = 5000;
@@ -15,71 +16,33 @@ public static class Program
             Console.WriteLine("you must provide an ip address to ping");
             return;
         }
-
-        // TODO: allow for multiple servers, or generated list from a config
-
-        var listOfServersToPing = args;
-
-        var table = new Table();
-        table.Caption($"Refreshing every five seconds");
-        table.AddColumn("Server to Ping");
-        table.AddColumn("IP Address");
-        table.AddColumn("Ping Status");
-        table.AddColumn("RoundTrip Time");
-        table.AddColumn("Total Number of times pinged");
-        table.AddColumn("Total Number of failed pings");
-        table.AddColumn("Percentage of Success");
-
-        // TODO: create an object for all the ping response information, failures, etc
-        double loopCount = 0;
-        double failureCount = 0;
-        double percentageOfSuccess = 0;
-        while (true)
+        
+        var listOfServersToPing = args.Select(server => new ServerPingService(server)).ToList();
+        PingListOfServers(listOfServersToPing);
+    }
+        
+        // TODO: Allow for event handling so that we have a do while until eventSignal
+        // TODO: Async
+        private static void PingListOfServers(List<ServerPingService> listOfServersToPing)
         {
-            AnsiConsole.Live(table)
-                .AutoClear(true)
-                .Start(ctx =>
-                {
-                    List<(string, PingReply?)> serverPingResponses = [];
-                    serverPingResponses.AddRange(from serverToPing in listOfServersToPing
-                                                 let serverPingResponse
-                        = PingService.PingServer(serverToPing)
-                                                 select (serverToPing, serverPingResponse));
+            var table = ServerPingService.TableBuilder();
 
-                    loopCount++;
-                    percentageOfSuccess = (loopCount - failureCount) / loopCount * 100;
-
-                    foreach (var (serverToPing, pingReplyFromPingService) in serverPingResponses)
+            while (true)
+            {
+                AnsiConsole.Live(table)
+                    .AutoClear(true)
+                    .Start(ctx =>
                     {
-                        List<string> rowBuilder = [];
-
-                        if (pingReplyFromPingService is null)
+                        foreach (var serverToPing in listOfServersToPing)
                         {
-                            Console.WriteLine("Failed To Ping server...");
-                            AnsiConsole.MarkupLine("Failed To Ping server...");
-
-                            // TODO: this is a bug as failures are global right now.
-                            failureCount++;
-                        }
-                        else
-                        {
-                            rowBuilder.Add(serverToPing);
-                            rowBuilder.Add(pingReplyFromPingService.Address.ToString());
-                            rowBuilder.Add((pingReplyFromPingService.Status.ToString()));
-                            rowBuilder.Add((pingReplyFromPingService.RoundtripTime.ToString()));
+                            serverToPing.PingServer();
+                            table.AddRow(serverToPing.ArrayOfLatestPingInformation());
                         }
 
-                        rowBuilder.Add(loopCount.ToString(CultureInfo.CurrentCulture));
-                        rowBuilder.Add(failureCount.ToString(CultureInfo.CurrentCulture));
-                        rowBuilder.Add(percentageOfSuccess.ToString(CultureInfo.CurrentCulture));
-
-                        table.AddRow(rowBuilder.ToArray());
-                    }
-
-                    ctx.Refresh();
-                    Thread.Sleep(FiveMilliseconds);
-                });
-            table.Rows.Clear();
+                        ctx.Refresh();
+                        Thread.Sleep(FiveMilliseconds);
+                    });
+                table.Rows.Clear();
+            }
         }
     }
-}
---
+++ SpectreServerStatus/ServerPingService.cs
@@ -0,0 +1,86 @@
+using System.Globalization;
+using System.Net.NetworkInformation;
+using Spectre.Console;
+
+namespace SpectreServerStatus;
+
+public class ServerPingService(string serverToPing)
+{
+    private string ServerToPing { get; init; } = serverToPing;
+    private PingReply? LatestPingReply { get; set; }
+    private string PingReplyIpAddress { get; set; } = string.Empty;
+    private string PingReplyStatus { get; set; } = string.Empty;
+    private string PingReplyRoundTripTime { get; set; } = string.Empty;
+    private double PingCount { get; set; }
+    private double PingFailureCount { get; set; }
+
+    private double PingSuccessPercentage => (PingCount - PingFailureCount) / PingCount * 100;
+    private const int DefaultPingTimeout = 120;
+
+    /// <summary>
+    /// Takes the relevant information of the PingReply and returns the array of information
+    /// </summary>
+    /// <returns>In order of [ServerToPing, PingReplyIpAddress, PingReplyStatus PingReplyRoundTripTime,
+    /// PingCount, PingFailureCount, PingSuccessPercentage]</returns>
+    public string[] ArrayOfLatestPingInformation()
+    {
+        return 
+        [
+            ServerToPing,
+            PingReplyIpAddress,
+            PingReplyStatus,
+            PingReplyRoundTripTime,
+            PingCount.ToString(CultureInfo.InvariantCulture),
+            PingFailureCount.ToString(CultureInfo.InvariantCulture),
+            PingSuccessPercentage.ToString(CultureInfo.InvariantCulture)
+        ];
+    }
+    /// <summary>
+    /// Pings the ServerToPing attribute and updates the other attributes
+    /// </summary>
+    public void PingServer()
+    {
+        Ping pingSender = new();
+
+        LatestPingReply = pingSender.Send(ServerToPing, DefaultPingTimeout);
+        
+        UpdatePingReplyInformation();
+        UpdatePingCounts();
+    }
+
+    /// <summary>
+    /// Builds an ordered table that PingService expects for proper user of ArrayOfLatestPingInformation()
+    /// </summary>
+    /// <returns> A Spectre.Console.Table object with columns pre allocated, and no rows initialized</returns>
+    public static Table TableBuilder()
+    {
+        var table = new Table();
+        table.Caption($"Refreshing every five seconds");
+        table.AddColumn("Server to Ping");
+        table.AddColumn("IP Address");
+        table.AddColumn("Ping Status");
+        table.AddColumn("RoundTrip Time");
+        table.AddColumn("Total Number of times pinged");
+        table.AddColumn("Total Number of failed pings");
+        table.AddColumn("Percentage of Success");
+
+        return table;
+    }
+
+    private void UpdatePingCounts()
+    {
+        PingCount++;
+        
+        if (LatestPingReply is null || LatestPingReply.Status != IPStatus.Success)
+        {
+            PingFailureCount++;
+        }
+    }
+
+    private void UpdatePingReplyInformation()
+    {
+        PingReplyIpAddress = LatestPingReply is not null ? LatestPingReply.Address.ToString() : string.Empty;
+        PingReplyStatus = LatestPingReply is not null ? LatestPingReply.Status.ToString() : string.Empty;
+        PingReplyRoundTripTime = LatestPingReply is not null ? LatestPingReply.RoundtripTime.ToString() : string.Empty;
+    }
+}
